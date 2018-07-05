16f89791f43280e04d0b07e9d580d0da0258cbf9
skyele <skyele@sjtu.edu.cn>
2018-07-05 20:51:13 +0800

    learn rabbitmq

---
+++ backend/MicroService/Rabbitmq/src/main/java/com/testmq/rabbitmq/Config/RabbitConfig.java
@@ -0,0 +1,51 @@
+package com.testmq.rabbitmq.Config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public final static String QUEUE_NAME = "signup-queue";
+    public final static String EXCHANGE_NAME = "signup-exchange";
+    public final static String ROUTING_KEY = "signup-key";
+
+    // 创建队列
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    // 创建一个 topic 类型的交换器
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    // 使用路由键（routingKey）把队列（Queue）绑定到交换器（Exchange）
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("192.168.238.132", 5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
+
+}
