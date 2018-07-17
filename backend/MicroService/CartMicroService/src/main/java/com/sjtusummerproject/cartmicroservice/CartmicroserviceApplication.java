fb781d85828cd9434d87fd36a047467838e3392f
skyele <skyele@sjtu.edu.cn>
2018-07-17 11:34:45 +0800

    finish cart microservice

---
+++ backend/MicroService/CartMicroService/src/main/java/com/sjtusummerproject/cartmicroservice/CartmicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.sjtusummerproject.cartmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartmicroserviceApplication.class, args);
+	}
+}
