ca83d7a4d93af25c5ac652ab01fddc5bb4523c93
skyele <skyele@sjtu.edu.cn>
2018-07-18 16:43:42 +0800

    add detail entity

--- backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/DataModel/Dao/UserDetailRepository.java
+++ backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/DataModel/Dao/UserDetailRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import sjtusummerproject.usermicroservice.DataModel.Domain.UserDetailEntity;
 
 public interface UserDetailRepository extends CrudRepository<UserDetailEntity,Long> {
+    public UserDetailEntity findById(Long id);
 }
