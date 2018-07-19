5ad53b747ed8202e41663bde5a6b0a3e3ce33975
skyele <skyele@sjtu.edu.cn>
2018-07-19 15:52:56 +0800

    add picture microservice

--- backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/UsermicroserviceApplication.java
+++ backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/UsermicroserviceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableMongoAuditing
 @EnableMongoRepositories
+@EnableTransactionManagement
 public class UsermicroserviceApplication {
 
 	public static void main(String[] args) {
