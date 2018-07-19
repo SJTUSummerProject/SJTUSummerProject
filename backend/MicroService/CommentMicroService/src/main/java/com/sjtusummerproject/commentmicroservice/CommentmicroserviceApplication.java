64578be770a4aa6faf14f52d53e97543a107c72f
skyele <skyele@sjtu.edu.cn>
2018-07-19 15:58:26 +0800

    create comment service

---
+++ backend/MicroService/CommentMicroService/src/main/java/com/sjtusummerproject/commentmicroservice/CommentmicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.sjtusummerproject.commentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentmicroserviceApplication.class, args);
+	}
+}
