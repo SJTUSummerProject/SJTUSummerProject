d10691db9608c8bd57c74fce83fabf9cb30183d3
skyele <skyele@sjtu.edu.cn>
2018-07-19 22:16:12 +0800

    polish comment service

---
+++ backend/MicroService/CommentMicroService/src/main/java/com/sjtusummerproject/commentmicroservice/Config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.sjtusummerproject.commentmicroservice.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfiguration createConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", createConfig());
+        return new CorsFilter(source);
+    }
+}
+
