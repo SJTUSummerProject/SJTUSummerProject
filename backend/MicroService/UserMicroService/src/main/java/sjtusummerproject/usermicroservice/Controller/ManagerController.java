f2384409f0fd4763f50f12365f062998e10352ae
skyele <skyele@sjtu.edu.cn>
2018-08-24 19:09:43 +0800

    add manager to user microservice

---
+++ backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/Controller/ManagerController.java
@@ -0,0 +1,60 @@
+package sjtusummerproject.usermicroservice.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+import sjtusummerproject.usermicroservice.DataModel.Domain.UserEntity;
+import sjtusummerproject.usermicroservice.Service.ManageUserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value="/Manager")
+public class ManagerController {
+    @Autowired
+    ManageUserService manageUserService;
+
+    @Value("${user.page.size}")
+    private int PageSize;
+    @Value("${user.page.offset}")
+    private int PageOffset;
+
+    /*按照时间顺序 递减排列*/
+    public Pageable createPageable(HttpServletRequest request){
+        return new PageRequest(Integer.parseInt(request.getParameter("pagenumber"))-PageOffset, PageSize, new Sort(Sort.Direction.DESC, "id"));
+    }
+
+    @RequestMapping(value="/Delete")
+    void deleteUser(@RequestParam(name = "id")Long userid){
+        manageUserService.DeleteUserOption(userid);
+    }
+
+    @RequestMapping(value = "/QueryById")
+    UserEntity queryUserById(@RequestParam(name = "id")Long userid){
+        return manageUserService.QueryUserByIdOption(userid);
+    }
+
+    @RequestMapping(value = "/QueryBatch")
+    Page<UserEntity> queryBatchUser(HttpServletRequest request){
+        return manageUserService.QueryBatch(createPageable(request));
+    }
+
+    @RequestMapping(value = "/UpdateStatus")
+    String updateUserStatus(@RequestParam(name = "id")Long userid,
+                                @RequestParam(name = "status")String status){
+        UserEntity afterUpdateUser = manageUserService.UpdateUserStatusOptionById(userid,status);
+        return afterUpdateUser==null ? "user not exist" : "ok";
+    }
+
+    @RequestMapping(value = "/UpdatePassword")
+    String updateUserPassword(@RequestParam(name = "id")Long userid,
+                              @RequestParam(name = "password")String password){
+        UserEntity afterUpdateUser = manageUserService.UpdateUserPasswordOptionById(userid,password);
+        return afterUpdateUser==null? "user not exist" : "ok";
+    }
+}
--- backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/Service/ManageUserService.java
+++ backend/MicroService/UserMicroService/src/main/java/sjtusummerproject/usermicroservice/Service/ManageUserService.java
@@ -1,5 +1,7 @@
 package sjtusummerproject.usermicroservice.Service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestParam;
 import sjtusummerproject.usermicroservice.DataModel.Domain.UserEntity;
 
@@ -12,4 +14,9 @@ public interface ManageUserService {
     public UserEntity UpdateUserStatusOption(String username, String status);
     public UserEntity UpdateUserPasswordOption(String username, String password);
     public UserEntity UpdateUserAuthorityOption(String username,String authority);
+
+    public UserEntity UpdateUserStatusOptionById(Long id, String status);
+    public UserEntity UpdateUserPasswordOptionById(Long id, String password);
+    public UserEntity UpdateUserAuthorityOptionById(Long id, String authority);
+    public Page<UserEntity> QueryBatch(Pageable pageable);
 }
